4aa19a9fafead56d643d37413693b673693d4ad7
amal-vk-das <amal.das@jobinandjismi.com>
2024-07-09 13:12:48 +0530

    3D carDisplay threejs files upload

---
+++ script.js
@@ -0,0 +1,92 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.161/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.161/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.161/examples/jsm/loaders/GLTFLoader.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.set(2, 2, 4);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.06;
+controls.enablePan = false;
+controls.minDistance = 2;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+groundGeometry.rotateX(-Math.PI / 2);
+const groundMaterial = new THREE.MeshStandardMaterial({
+  color: 0x000000,
+  side: THREE.DoubleSide,
+});
+const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+groundMesh.castShadow = false;
+groundMesh.receiveShadow = true;
+scene.add(groundMesh);
+
+const spotLight = new THREE.SpotLight(0xffffff, 3500, 100, 0.2, 1);
+spotLight.position.set(0, 25, 0);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -0.0001;
+scene.add(spotLight);
+
+const spotLight4 = new THREE.SpotLight(0xffffff, 4500, 100, 0.2, 1);
+spotLight4.position.set(0, 15, 0);
+spotLight4.castShadow = true;
+spotLight4.shadow.bias = -0.0001;
+scene.add(spotLight4);
+
+const spotLight5 = new THREE.SpotLight(0xffffff, 4500, 100, 0.2, 1);
+spotLight5.position.set(5, 15, 15);
+spotLight5.castShadow = true;
+spotLight5.shadow.bias = -0.0001;
+scene.add(spotLight5);
+
+const gltfLoader = new GLTFLoader();
+const url = "public/porsche_911_turbo_s__takeover/scene.gltf";
+gltfLoader.load(url, (gltf) => {
+  const root = gltf.scene;
+  root.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  root.position.set(0, 0, 0);
+  scene.add(root);
+});
+
+//ambientLight
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", handleWindowResize, false);
